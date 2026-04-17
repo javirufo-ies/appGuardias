9472b27934ac1155a7b970fc59d1393f5907add3
Javi Rufo <javirufo@educarex.es>
2026-04-17 14:22:10 +0200

    Eliminación API Google

--- admin/config.php
+++ admin/config.php
@@ -10,11 +10,3 @@ define('DB_PASS', 'primuxtech'); // o vacío, según MAMP
 
 define('google_calendar', 'https://calendar.google.com/calendar/ical/c_caea1e0b1c3d1f275836e1e0acb4c80891d4967f32bac019dfabde4bf43bef63@group.calendar.google.com/public/basic.ics');
 
-define('google_client_id', '867434824225-rcaa3i6usmkhsr2djfdrehc9ub6sauaj.apps.googleusercontent.com');
-define('google_client_secret', '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');
-define('google_redirect_uri', 'http://guardavalle.iesvjp.es/oauth2callback.php');
-define('google_config', [
-    'client_id' => google_client_id,
-    'client_secret' => google_client_secret,
-    'redirect_uri' => google_redirect_uri
-]);
